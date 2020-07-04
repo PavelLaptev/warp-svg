01b029971903084918b68d5ec8e7a48f1b59da36
Pavel <laptev.graphics@gmail.com>
2020-07-04 18:52:26 +0200

    build issues

--- dist/loader.b5849e04.js
+++ dist/loader.b5849e04.js
@@ -154,7 +154,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51163" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62234" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
